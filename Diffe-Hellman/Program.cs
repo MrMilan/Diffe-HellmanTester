5e723002469cbb8ce2b6fd3761752a4a43b321b7
MrMilan <polacek.emilan@gmail.com>
2014-05-14 16:10:29 +0200

    Udelano prvotni nacitani priprava pocetnich vzorcu

--- Diffe-Hellman/Program.cs
+++ Diffe-Hellman/Program.cs
@@ -9,6 +9,51 @@ namespace Diffe_Hellman
     {
         static void Main(string[] args)
         {
+            int a, b;
+            ///prime number 
+            int p = 13;
+            ///base
+            int g = 5;
+            do
+            {
+                Console.WriteLine("Alice enter your secret number \"a\" (1 to 16)");
+                a = ValidateReadNumber(Console.ReadLine());
+            } while (a < 0 || a > 17);
+
+
+            do
+            {
+                Console.WriteLine("Bob enter your secret number \"b\" (1 to 16)");
+                b = ValidateReadNumber(Console.ReadLine());
+            } while (b < 0 || b > 17);
+
+
+        }
+
+        public static int ValidateReadNumber(string valueNumber)
+        {
+            int number = 0;
+            try
+            {
+                number = Convert.ToInt32(valueNumber);
+            }
+            catch (Exception exc)
+            {
+
+                throw new ArgumentException("Not valid input" + exc.Message);
+            }
+
+            return number;
+        }
+
+        public static int NumerateBigChar(int primeNumber, int gBase, int exponent)
+        {
+            return Convert.ToInt32(Math.Pow(gBase, exponent)) % primeNumber;
+        }
+
+        public static int NumerateSsecret(int primeNumber, int bigChar, int exponent)
+        {
+            return Convert.ToInt32(Math.Pow(bigChar, exponent)) % primeNumber;
         }
     }
 }
