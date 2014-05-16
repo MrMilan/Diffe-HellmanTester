109cb5b1c223ca949eb94ea04b6f9994cf1fdbac
MrMilan <polacek.emilan@gmail.com>
2014-05-16 10:56:11 +0200

    Mala uprava komunikace s uživatelem

--- Diffe-Hellman/Program.cs
+++ Diffe-Hellman/Program.cs
@@ -9,39 +9,53 @@ namespace Diffe_Hellman
     {
         static void Main(string[] args)
         {
-            double privateA, privateB,publicA,publicB,secretA,secretB;
+            double privateA=0, privateB,publicA,publicB,secretA,secretB;
             ///prime number 
             int primeNumPub = 23;
             ///base
             int grupaNumPub = 5;
             Random rnd = new Random();
 
-            int prim;
-            
+            Console.ForegroundColor = ConsoleColor.Green;
             do
             {
-               prim= rnd.Next(1, 100);
-                
-            } while (IsItPrime(prim));
+                primeNumPub = rnd.Next(2, 100);
+
+            } while (!IsItPrime(primeNumPub));
+            bool errCount = false;
             do
             {
+                if (errCount) { Console.WriteLine("NUMBER MUST BE BETWEEN 1 AND 16 !!!!!"); }
+                if (privateA == 42) { EasterEgg(); }
                 Console.WriteLine("Alice enter your secret number \"a\" (1 to 16)-private key");
+                
                 privateA = ValidateReadNumber(Console.ReadLine());
+                errCount = true;
             } while (privateA < 0 || privateA > 17);
 
-
+            errCount = false;
             do
             {
+                if (errCount) { Console.WriteLine("NUMBER MUST BE BETWEEN 1 AND 16 !!!!!");  }
                 Console.WriteLine("Bob enter your secret number \"b\" (1 to 16)-private key");
                 privateB= ValidateReadNumber(Console.ReadLine());
+                errCount=true;
             } while (privateB < 0 || privateB > 17);
-            Console.WriteLine("Exchange of public keys");
+
+            Console.WriteLine("\nGenerating public key. Public prime is {0}. Public Grupa is {1}.\n", primeNumPub, grupaNumPub);
             publicA = NumeratePublicKey(primeNumPub, grupaNumPub, privateA);
             publicB = NumeratePublicKey(primeNumPub, grupaNumPub, privateB);
+
+            Console.WriteLine("\nExchange of public keys");
             Console.WriteLine("Alice get public key B = {0},Bob get public key A = {1}",publicB,publicA);
+            
+            Console.WriteLine("\nGenerating secrets");
             secretA = NumerateSsecret(primeNumPub, publicB, privateA);
             secretB = NumerateSsecret(primeNumPub, publicA, privateB);
             Console.WriteLine("Secret of Alice {0} and secret Bob {1}",secretA,secretB);
+            
+
+            Console.WriteLine("\nFor end of program press any key");
             Console.ReadKey();
 
         }
@@ -83,6 +97,24 @@ namespace Diffe_Hellman
 
             }
             return true;
+        }
+
+        public static void EasterEgg()
+        {
+            Console.Clear();
+            Console.WriteLine("Alice welcome in wonderland, follow white rabbit to hole");
+            Console.WriteLine(" /\`\ /\ \
+          `-\ ///`-'
+            /. .\
+            \ Y /
+            /'-'\
+           | \_/ |
+          /\  Y  /\
+         (  `-'-`  )
+      jgs / /---\ \
+         (_/     \_)")
+
+            Console.Clear();
         }
 
     }
